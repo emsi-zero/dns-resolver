a49e1acba183999f12b674d0f635f139853f2510
emsi-zero <emad.fdws@gmail.com>
2024-08-28 13:09:35 +0330

    add query header

--- internal/dns/query.go
+++ internal/dns/query.go
@@ -1,2 +1,27 @@
 package dns
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Header struct {
+	ID      uint16
+	Flags   uint16
+	QdCount uint16
+	AnCount uint16
+	NsCount uint16
+	ArCount uint16
+}
+
+func ParseHeader(reader *bytes.Reader) {
+	var header Header
+	
+	binary.Read(reader, binary.BigEndian, header.ID)
+	binary.Read(reader, binary.BigEndian, header.Flags)
+	binary.Read(reader, binary.BigEndian, header.QdCount)
+	binary.Read(reader, binary.BigEndian, header.AnCount)
+	binary.Read(reader, binary.BigEndian, header.NsCount)
+	binary.Read(reader, binary.BigEndian, header.ArCount)
+}
+
